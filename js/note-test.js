68f60c7c2cb7e8b6ea7d2b6a914d0962bb3c04c1
aomkhamphaung <akhamphaung@gmail.com>
2023-08-20 12:20:08 +0630

    node testing

---
+++ js/note-test.js
@@ -0,0 +1,13 @@
+const myName = 'Aom Kham Phaung'
+console.log(myName);
+
+let x = 5;
+let y = 10;
+console.log(x + y);
+
+const arr = ['a', 'b', 'c'];
+console.log(arr);
+
+for(x of arr){
+    console.log(x);
+}
